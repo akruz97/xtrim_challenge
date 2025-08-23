a8fb92e963c3027bdf7ed29bb14b7630e8607194
Jose Cruz <jose.cruzal@outlook.com>
2025-08-22 21:55:45 -0500

    init project

---
+++ backend/xtrim_api/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=100)
+    base_price = models.DecimalField(max_digits=10, decimal_places=4)
+    mb_included = models.IntegerField(help_text="Data limit in MB")
+    mb_price = models.DecimalField(max_digits=10, decimal_places=4, help_text="Price per MB over limit")
+    minute_price = models.DecimalField(max_digits=10, decimal_places=4, help_text="Price per minute")
+    duration = models.IntegerField(help_text="Duration in days")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
+class Client(models.Model):
+    identification = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class PlanClient(models.Model):
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    start_date = models.DateField()
+
+
+class Consumption(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date = models.DateField()
+    mb_used = models.IntegerField(default=0)
+    minutes_used = models.IntegerField(default=0)
+
+class ConsumptionType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
